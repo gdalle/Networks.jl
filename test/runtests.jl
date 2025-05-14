13194447ff25ad92ab144040f0ff7a9e7df080b2
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-05-14 13:09:55 +0200

    Start prototyping `Taggable` tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using SafeTestsets
 
 @testset "Unit" verbose = true begin
     @safetestset "Network" include("unit/network.jl")
+    @safetestset "Taggable" include("unit/taggable.jl")
 end
 
 @testset "Integration" verbose = true begin end
